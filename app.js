8ddacd10c3f686ccdc9e4416a67e5bf98b695f91
richard141271 <68450987+richard141271@users.noreply.github.com>
2025-12-12 05:36:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ function saveKube(data){ const a=getBikuber(); a.unshift(data); localStorage.set
 
 // små hjelpefunksjoner
 function escapeHtml(s){ return String(s||'').replace(/[&<>"']/g, c=>({ '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;' })[c]); }
+
+/* INSPEKSJONER - localStorage */
+function getInspeksjoner(){ return JSON.parse(localStorage.getItem('inspeksjoner')||'[]'); }
+function saveInspeksjoner(arr){ localStorage.setItem('inspeksjoner', JSON.stringify(arr)); }
+function saveInspeksjon(obj){ const a=getInspeksjoner(); a.unshift(obj); saveInspeksjoner(a); }
+function removeInspeksjon(id){ const a=getInspeksjoner().filter(i=>i.id!==id); saveInspeksjoner(a); }
+
+/* hjelp: slette inspeksjon wrapper */
+function deleteInspeksjon(id){ removeInspeksjon(id); }
+
+/* utvider saveKube for å sette opprettet-tid hvis ikke */
+function saveKube(obj){
+  const a=getBikuber();
+  if(!obj.opprettet) obj.opprettet = Date.now();
+  a.unshift(obj); localStorage.setItem('kuber', JSON.stringify(a));
+}
